69c9a362bbde334766816f7206179fe5d527aed0
Jie Yan <jie.yan.cst@gmail.com>
2022-04-08 16:52:07 +0800

    Finished PA2-3 V2 15/50 TLE

--- PA2/2-3 ⑨的完美文本编辑器/main.cpp
+++ PA2/2-3 ⑨的完美文本编辑器/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  2-3 ⑨的完美文本编辑器 V1
+//  2-3 ⑨的完美文本编辑器 V2
 //
 //  Created by Yan on 2022/3/16.
 //
@@ -17,13 +17,13 @@ struct ListNode { //列表节点模板类（以双向链表形式实现）
     ListNode() {}; //创建头哨兵、尾哨兵时使用此种构造函数
     ListNode(char e, ListNode* p = NULL, ListNode* s = NULL) : data(e), pred(p), succ(s) {}; //构造器
 //操作接口
-//    ListNode* insertAsPred (char const& e) //紧靠当前节点之前插入新节点
-//    {
-//        ListNode* x = new ListNode(e, pred, this); //创建新节点
-//        pred->succ = x;
-//        pred = x; //设置正向链接？
-//        return x; //返回新节点的位置
-//    };
+    ListNode* insertAsPred (char const& e) //紧靠当前节点之前插入新节点
+    {
+        ListNode* x = new ListNode(e, pred, this); //创建新节点
+        pred->succ = x;
+        pred = x; //设置正向链接？
+        return x; //返回新节点的位置
+    };
     ListNode* insertAsSucc (char const& e) //紧随当前节点之后插入新节点
     {
         ListNode* x = new ListNode(e, this, succ); //创建新节点
@@ -59,10 +59,10 @@ public:
     {
         return p->insertAsSucc(e);
     };
-//    ListNode* insertL(ListNode* p, char const& e) //e从p的左侧（前驱）插入
-//    {
-//        return p->insertAsPred(e);
-//    };
+    ListNode* insertL(ListNode* p, char const& e) //e从p的左侧（前驱）插入
+    {
+        return p->insertAsPred(e);
+    };
     ListNode* removeR (ListNode* p) //删除合法节点p
     {
         p->pred->succ = p->succ; //改前驱的后继
@@ -94,13 +94,17 @@ int main() {
     List list;
     ListNode* pred = list.header;
     char text = getchar();
+    int length = 0;
     while (text != '\n')
     {
         pred = list.insertR(pred, text);
         text = getchar();
+        length++;
     }
     ListNode* cursorL = new ListNode(0, list.header, list.header->succ); //左光标
     ListNode* cursorR = new ListNode(0, list.trailer->pred, list.trailer); //右光标
+    int curPosL = 0;
+    int curPosR = length;
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
@@ -118,6 +122,7 @@ int main() {
                     cout << "T" << endl;
                     cursorL->succ = cursorL->pred;
                     cursorL->pred = cursorL->pred->pred;
+                    curPosL--;
                 }
                 else
                     cout << "F" << endl;
@@ -130,6 +135,7 @@ int main() {
                     cout << "T" << endl;
                     cursorR->succ = cursorR->pred;
                     cursorR->pred = cursorR->pred->pred; //两个赋值式不能反
+                    curPosR--;
                 }
                 else
                     cout << "F" << endl;
@@ -147,6 +153,7 @@ int main() {
                     cout << "T" << endl;
                     cursorL->pred = cursorL->succ;
                     cursorL->succ = cursorL->succ->succ;
+                    curPosL++;
                 }
                 else
                     cout << "F" << endl;
@@ -159,6 +166,7 @@ int main() {
                     cout << "T" << endl;
                     cursorR->pred = cursorR->succ;
                     cursorR->succ = cursorR->succ->succ;
+                    curPosR++;
                 }
                 else
                     cout << "F" << endl;
@@ -177,6 +185,9 @@ int main() {
                 cursorL->pred = list.insertR(cursorL->pred, e);
                 if (cursorR->pred->succ == cursorL->pred) //若cursorR的前驱与新加入节点相链，则需更新cursorR的前驱为新加入节点
                     cursorR->pred = cursorL->pred;
+                if (curPosR >= curPosL)
+                    curPosR++;
+                curPosL++;
             }
             else
             {
@@ -185,6 +196,9 @@ int main() {
                 cursorR->pred = list.insertR(cursorR->pred, e);
                 if (cursorL->pred->succ == cursorR->pred) //若cursorL的前驱与新加入节点相链，则需更新cursorL的前驱为新加入节点
                     cursorL->pred = cursorR->pred;
+                if (curPosL >= curPosR)
+                    curPosL++;
+                curPosR++;
             }
             continue;
         }
@@ -198,6 +212,8 @@ int main() {
                 {
                     cout << "T" << endl;
                     cursorL->succ = list.removeR(cursorL->succ);
+                    if (curPosR > curPosL)
+                        curPosR--;
                 }
                 else
                     cout << "F" << endl;
@@ -208,6 +224,8 @@ int main() {
                 {
                     cout << "T" << endl;
                     cursorR->succ = list.removeR(cursorR->succ);
+                    if (curPosL > curPosR)
+                        curPosL--;
                 }
                 else
                     cout << "F" << endl;
@@ -216,26 +234,12 @@ int main() {
         }
         else if (opt == 'R') //反向反data即可
         {
-            ListNode* left = cursorL->succ;
-            ListNode* right = cursorL->succ;
-            bool valid = true;
-            int sum = 0;
-            while (right != cursorR->pred->succ)
-            {
-                if (right == list.trailer)
-                {
-                    valid = false;
-                    break;
-                }
-                sum++;
-                right = right->succ;
-            }
-            if (left == right) //左端与右端重合
-                valid = false;
-            if (valid == true)
+            if (curPosR>curPosL)
             {
                 cout << "T" << endl;
-                list.reverse(left, right->pred, sum);
+                ListNode* left = cursorL->succ;
+                ListNode* right = cursorR->succ;
+                list.reverse(left, right->pred, curPosR - curPosL);
             }
             else
                 cout << "F" << endl;
